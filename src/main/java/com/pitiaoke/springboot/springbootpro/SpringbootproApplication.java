50c8cf4a816b9039a45674d2c9ce17c5755ef53a
liyuanqian001 <2239386142@qq.com>
2019-04-25 23:34:16 +0800

    my first springbootpro from idea

---
+++ src/main/java/com/pitiaoke/springboot/springbootpro/SpringbootproApplication.java
@@ -0,0 +1,14 @@
+package com.pitiaoke.springboot.springbootpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class SpringbootproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootproApplication.class, args);
+    }
+
+}
